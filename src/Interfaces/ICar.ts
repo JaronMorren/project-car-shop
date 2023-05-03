8b58d16a0b4bbad81437f45a5ab56641360ae0ff
Jaron Morren <jaronmorren@gmail.com>
2023-05-03 20:26:34 -0300

    created ICar interface

---
+++ src/Interfaces/ICar.ts
@@ -0,0 +1,11 @@
+interface ICar {
+  id?: string;
+  model: string;
+  year: number;
+  color: string;
+  status?: boolean | false;
+  buyValue: number;
+  doorsQty: number;
+  seatsQty: number;
+}
+export default ICar;
