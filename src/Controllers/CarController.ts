7c2028d72c2f5269b5ec8338bd6ecd0d92094214
Jaron Morren <jaronmorren@gmail.com>
2023-05-04 20:54:17 -0300

    added getallCars and getCarByID to carController

--- src/Controllers/CarController.ts
+++ src/Controllers/CarController.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import { isValidObjectId } from 'mongoose';
 import ICar from '../Interfaces/ICar';
 import CarService from '../Services/CarService';
 
@@ -20,6 +21,30 @@ class CarController {
     try {
       const newCar = await this.service.createCar(car);
       return this.response.status(201).json(newCar);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+  public async getAllCars() {
+    try {
+      const cars = await this.service.getAllCars();
+      return this.response.status(200).json(cars);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+
+  public async getCarByID() {
+    const { id } = this.request.params;
+    try {
+      if (!isValidObjectId(id)) {
+        return this.response.status(422).json({ message: 'Invalid mongo id' });
+      }
+      const car = await this.service.getCarByID(id);
+      if (car !== null) {
+        return this.response.status(200).json(car);
+      }
+      return this.response.status(404).json({ message: 'Car not found' });
     } catch (error) {
       this.next(error);
     }
