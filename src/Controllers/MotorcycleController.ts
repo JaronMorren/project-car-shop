8f6223cb3989add02ff17ad4860f5d33d24e6238
Jaron Morren <jaronmorren@gmail.com>
2023-05-06 22:04:23 -0300

    added getAll and getID methods to MotorcycleController

--- src/Controllers/MotorcycleController.ts
+++ src/Controllers/MotorcycleController.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import { isValidObjectId } from 'mongoose';
 import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotorcycleService from '../Services/MotorcycleService';
 
@@ -22,6 +23,30 @@ class MotorcycleController {
       return this.response.status(201).json(newMotorcycle);
     } catch (error) {
       return this.next(error);
+    }
+  }
+  public async getAllMotorcycles() {
+    try {
+      const motorcycles = await this.service.getAllMotorcycles();
+      return this.response.status(200).json(motorcycles);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+
+  public async getMotorcycleByID() {
+    const { id } = this.request.params;
+    try {
+      if (!isValidObjectId(id)) {
+        return this.response.status(422).json({ message: 'Invalid mongo id' });
+      }
+      const motorcycle = await this.service.getMotorcycleByID(id);
+      if (motorcycle !== null) {
+        return this.response.status(200).json(motorcycle);
+      }
+      return this.response.status(404).json({ message: 'Motorcycle not found' });
+    } catch (error) {
+      this.next(error);
     }
   }
 }
