775bf7b093c6ff627ec4ce0c77723f7cb9283516
Jaron Morren <jaronmorren@gmail.com>
2023-05-06 22:03:58 -0300

    added getAll and getID methods to MotorcycleService

--- src/Services/MotorcycleService.ts
+++ src/Services/MotorcycleService.ts
@@ -8,5 +8,23 @@ class MotorcycleService {
     const newMotorcycle = await motorcycleODM.createVehicle(motorcycle);
     return new Motorcycle(newMotorcycle);
   }
+  public async getAllMotorcycles() {
+    const motorcycleODM = new MotorcycleODM();
+    const motorcycles = await motorcycleODM.getAllVehicles();
+    const motorcycleDomain = motorcycles.map((motorcycle) => { 
+      const newMotorcycle = new Motorcycle(motorcycle);
+      return newMotorcycle;
+    });
+    return motorcycleDomain;
+  }
+
+  public async getMotorcycleByID(id: string) {
+    const motorcycleODM = new MotorcycleODM();
+    const motorcycle = await motorcycleODM.getVehicleByID(id);
+    if (motorcycle) {
+      return new Motorcycle(motorcycle);
+    }
+    return null;
+  }
 }
 export default MotorcycleService;
